a64acb75dbe1c88bb3a388da42b1baf379c29d91
frame-code <moraisurcan@hotmail.com>
2025-02-02 21:27:15 -0500

    First form completed

--- JS/scripts.js
+++ JS/scripts.js
@@ -13,18 +13,18 @@ function createHeart() {
 }
 
 function stopAnimation(intervalID) {
-    const heartsContainer = d.querySelector(".hearts-container");
-    heartsContainer.style.animation = "floatUpDisappearing 2s linear forwards";
-    setTimeout(() => {
-        heartsContainer.remove(); 
-    }, 2000);
-    clearInterval(intervalID);
+  const heartsContainer = d.querySelector(".hearts-container");
+  heartsContainer.style.animation = "floatUpDisappearing 2s linear forwards";
+  setTimeout(() => {
+    heartsContainer.remove();
+  }, 2000);
+  clearInterval(intervalID);
 }
 
-d.getElementById("btnShow").addEventListener("click", function (e) {
+d.querySelector(".btnShow").addEventListener("click", function (e) {
   e.preventDefault(); // Prevent the form from submitting
 
-  let timeToStop = 4000;
+  let timeToStop = 1000;
   let intervalID = setInterval(createHeart, 100);
   setTimeout(() => {
     stopAnimation(intervalID);
@@ -32,6 +32,23 @@ d.getElementById("btnShow").addEventListener("click", function (e) {
 
   // Show the message after the hearts stop floating
   setTimeout(() => {
-    document.querySelector(".love-message").style.display = "block";
+    d.querySelector(".love-message").style.display = "block";
+    d.querySelector(".formDayMet").style.display = "block";
   }, timeToStop);
+});
+
+d.querySelector(".btnFollow1").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let dayMet = d.querySelector(".inputDayMet").value;
+  if (dayMet == "25") {
+    d.querySelector(".messageErrorOrCorrect").innerHTML =
+      "<br/>Correct! 🎉🎉🎉";
+    setTimeout(() => {
+      d.querySelector(".formDayMet").style.display = "none";
+      d.querySelector(".formDayAnniversary").style.display = "block";
+    }, 1500);
+  } else {
+    d.querySelector(".messageErrorOrCorrect").innerHTML = "<br/>Incorrect! 😒😒😒";
+  }
 });
